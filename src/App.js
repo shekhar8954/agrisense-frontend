2bfe8ef471d627fd4b99a44fb506b85545bf3f0a
shekhar8954 <shekhargupta500.sg@gmail.com>
2020-12-15 12:31:59 +0530

    testing-push-pull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import Login from './login/login'
 function App() {
   return (
     <div className="App">
       <h1>Welcome to AgriSense Project</h1>
+      <Login />
     </div>
   );
 }
